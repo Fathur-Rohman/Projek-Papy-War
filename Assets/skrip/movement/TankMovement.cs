7f2c8e462bfe94ccb9e88f281294e9d34440b4b6
Nox2903 <aryayudistira2903@gmail.com>
2024-10-28 19:53:18 +0700

    progres multiplayer

--- Assets/skrip/movement/TankMovement.cs
+++ Assets/skrip/movement/TankMovement.cs
@@ -5,15 +5,22 @@ public class TankMovement : MonoBehaviour
     public float moveSpeed = 5f;  // Kecepatan gerak tank
     public float rotationSpeed = 100f;  // Kecepatan rotasi tank
     public Rigidbody2D rb;  // Rigidbody dari tank
+    private Alteruna.Avatar _avatar;
 
     void Start()
     {
+        _avatar = GetComponent<Alteruna.Avatar>();
+
+        if  (_avatar.IsMe)
+            return;
         // Memastikan Rigidbody2D diambil dari objek tank
         rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
+        if  (_avatar.IsMe)
+            return;
         // Ambil input dari keyboard untuk pergerakan
         float moveDirection = Input.GetAxis("Vertical");  // W dan S untuk maju/mundur
         float rotateDirection = Input.GetAxis("Horizontal");  // A dan D untuk rotasi kiri/kanan
